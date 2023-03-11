343fad9527f4f631c8c47cae02eb03dbad2824bd
Владимир Войтенко <vladimirdev635@gmail.com>
2023-03-11 14:04:08 +0500

    fix-setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     author="Vladimir Vojtenko",
     author_email="vladimirdev635@gmail.com",
     license="MIT",
-    packages=["mcrs"],
-    package_data={"mcrs": ["py.typed"]},
+    packages=["mcrs", "mcrs.lifespan"],
+    package_data={"mcrs": ["py.typed"], "mcrs.lifespan": ["py.typed"]},
     long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
 )
