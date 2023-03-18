dfad0edaa59981b3b0b2602694ec6b1a159a81d1
Владимир Войтенко <vladimirdev635@gmail.com>
2023-03-18 16:45:35 +0500

    fix-imports

--- mcrs/cli/utils.py
+++ mcrs/cli/utils.py
@@ -1,8 +1,13 @@
 import importlib
+import os
+import sys
 from typing import Any
 
 
 def dynamic_import(name: str) -> Any:
+    working_directory = os.environ["PWD"]
+    if working_directory not in sys.path:
+        sys.path.insert(1, working_directory)
     components = name.split(".")
     variable = components[-1]
     mod = importlib.import_module(".".join(components[:-1]))
